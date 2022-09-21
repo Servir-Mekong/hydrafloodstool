a739cefe71a79a29de185994b734e25f563a23c9
Kamalh27 <plnkamalhosen@gmail.com>
2022-09-21 20:49:06 +0600

    add loading test file

--- hydrafloodviewer/urls.py
+++ hydrafloodviewer/urls.py
@@ -3,11 +3,12 @@ from django.urls import path
 from django.urls.conf import include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from django.views.generic import TemplateView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mapclient.urls')),
+    path('loaderio-33986448e3ce0d95a570f5c4e1a73b45.txt', TemplateView.as_view(template_name='loaderio-33986448e3ce0d95a570f5c4e1a73b45.txt', content_type='text/plain')),
 ]+static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
